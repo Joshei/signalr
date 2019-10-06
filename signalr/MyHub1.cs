bfeecfe871c63f840aa92bab66410898212b7fe5
JoshuaE <15404@LAPTOP-ABMUOFI9>
2019-10-06 17:43:28 -0400

    tidied up

--- signalr/MyHub1.cs
+++ signalr/MyHub1.cs
@@ -13,17 +13,7 @@ namespace SignalRChat
 
 
 {
-    //public static class UserHandler
-    //{
-    //public static HashSet<string> ConnectedIds = new HashSet<string>();
-
-    //}
-    //public class clients
-    //{
-    //    public string ConnectionId { get; set; }
-    //
-    //}
-
+    
 
 
     public class ChatHub : Hub
@@ -45,32 +35,26 @@ namespace SignalRChat
 
         public override Task OnConnected()
         {
-            // var id = Context.ConnectionId;
-            //CurrentConnections.Add(id);
+            
 
             return base.OnConnected();
         }
 
-        //return list of all active connections
-        //public List<string> GetAllActiveConnections()
-        //{
-        //    
-        //}
-
+        
         public void printturn()
         {
             string name = "";
             if (whoseturn == 0)
             {
 
-                //Clients.Client(ClientList[1].ConnectionId).printturn(name);
-                name = "Player Two";// ClientList[1].Name;
+                
+                name = "Player Two";
             }
             //second player
             if (whoseturn == 1)
             {
-                //Clients.Client(ClientList[0].ConnectionId).printturn(name);
-                // name = "Player One";// ClientList[0].Name;
+                
+                // name = "Player One";
             }
 
             if (whoseturn == 0)
@@ -82,24 +66,16 @@ namespace SignalRChat
                 whoseturn = 0;
             }
 
-            //get rid of whoseturn
+           
             Clients.Client(ClientList[0].ConnectionId).printname(name);
             Clients.Client(ClientList[1].ConnectionId).printname(name);
-            //return (name);
+           
 
         }
 
-
-
-
-
-
-
-
-
         public void register(string name)
         {
-            int myplayernumber = -1;
+            
 
             A_client.ConnectionId = Context.ConnectionId;
             A_client.Name = name;
@@ -107,23 +83,13 @@ namespace SignalRChat
 
             if (integer == 0)
             {
-                myplayernumber = 1;
+                Clients.Client(ClientList[0].ConnectionId).printinitial();
                 integer = integer + 1;
             }
             else if (integer == 1)
             {
                 Clients.Client(ClientList[1].ConnectionId).printinitial();
-                myplayernumber = 2;
             }
-
-            if (myplayernumber == 2)
-            {
-                //Clients.Client(ClientList[0].ConnectionId).pressbutton(message);
-                Clients.Client(ClientList[1].ConnectionId).printinitial();
-                //Clients.Client(ClientList[1].ConnectionId).printinitial(name);
-            }
-
-
 
 
         }
@@ -131,22 +97,14 @@ namespace SignalRChat
         //1 or zero
         public void play(string name, string message)
         {
-            //Debug.Write("play");
-            //for (int i = 0; i <= 1; i++)
-            //{
-            //if the correct user is presing the click number button
+            
             if (name == ClientList[whoseturn].Name)
             {
-                //controls whos turn is it and sends name for printing
-
-
-                //if (ClientList[i].ConnectionId == Context.ConnectionId)
-                //{
+                
                 if (message == "1")
                 {
 
-                    //disabled buttons!
-                    //Clients.AllExcept().pressbutton("1");
+                    
                     Clients.Client(ClientList[0].ConnectionId).pressbutton(message);
                     Clients.Client(ClientList[1].ConnectionId).pressbutton(message);
                 }
@@ -159,26 +117,6 @@ namespace SignalRChat
                 }
                 printturn();
             }
-
-
-
         }
-
-        //public void initial(string name)
-        //{
-        //    var a = 1;
-        //if (name == ClientList[0].Name)
-        //{
-
-
-
-        //    //get rid of whoseturn
-        //    Clients.Client(ClientList[0].ConnectionId).printname(name);
-        //    Clients.Client(ClientList[1].ConnectionId).printname(name);
-        //    //return (name);
-        //}
-
-        //}
     }
-
 }
